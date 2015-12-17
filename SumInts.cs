e8de8b99dc5ddd32744bf5f6354b98b69ffedf50
Matthew Bauer <krakenthedestroyer@gmail.com>
2015-12-16 23:31:42 -0600

    Chapter 5 Complete

---
+++ SumInts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumInts
+{
+    class SumInts
+    {
+        static void Main(string[] args)
+        {
+            double userinput;
+            double total = 0;
+            const double exitVariable = 999;
+
+            Console.WriteLine("Enter number to add:  ");
+            userinput = Convert.ToDouble(Console.ReadLine());
+
+            while (userinput != exitVariable)
+            {
+                total += userinput;
+                Console.WriteLine("Enter next amount, or " + exitVariable + " to sum it up ");
+                userinput = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine("Your total is {0}", total.ToString());
+            Console.WriteLine("Click enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
